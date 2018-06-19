7e05a769568f4417bd9316467782ab89c28573df
anithp <patelanith01@gmail.com>
2018-06-19 16:44:11 +0530

    Update AirowlAQI.ino

--- AirowlAQI.ino
+++ AirowlAQI.ino
@@ -1,4 +1,4 @@
-/*  Copyright (C)  2017  Anith Patel and Sohil Patel.
+/*  Copyright (C)  2018  Anith Patel and Sohil Patel.
     Permission is granted to copy, distribute and/or modify this document
     under the terms of the GNU Free Documentation License, Version 1.3
     or any later version published by the Free Software Foundation;
@@ -15,6 +15,7 @@
 
 byte PWM_PIN = 3;
 int pwm_value;
+int LED1 = A0;
 
 char auth[] = "Auth key"; //paste auth key of Blynk
 
@@ -32,6 +33,7 @@ void setup()
   Serial.begin(9600);
                      //led setup
   pinMode(PWM_PIN, INPUT);  
+  pinMode(LED1, OUTPUT);  
 }
 
 
@@ -40,7 +42,15 @@ void loop()
  Blynk.run();                               //calls blynk
  blePeripheral.poll();
  delay(500);
- WriteDataco2();   
+ WriteDataco2(); 
+ for(i=0; i<=256; i++){
+ analogWrite(LED1, i);
+ delay(100);    
+ }
+ for(i=256; i>=0; i--){
+ analogWrite(LED1, i);
+ delay(100);
+ }   
                                 //calls writedata function
 }
 
